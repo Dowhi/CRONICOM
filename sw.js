83a7f172dc12c30a607d2801d3ed89340ff86084
Dowhi <dowhi@ejemplo.com>
2026-02-16 07:54:45 +0100

    UI Fixes v1.0.6: Mute icon precise location at bottom-right, Quick Menu scroll forced

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'chronicon-v6';
+const CACHE_NAME = 'chronicon-v7';
 const ASSETS = [
     './',
     './index.html',
-    './calendar_medieval.html?v=1.0.5',
+    './calendar_medieval.html?v=1.0.6',
     './favicon.png',
     './manifest.json',
     './dragon.mp3'
